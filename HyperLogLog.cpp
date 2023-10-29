cf273f944a0c95e774d1f340de56e559e2ec5bb4
Luis-Valenzuela-Concha <lvalenzuel2020@udec.cl>
2023-10-28 22:34:58 -0300

    Se crea HyperLogLog

---
+++ HyperLogLog.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
+
+#include "HyperLogLog.h"
+
+HyperLogLog::HyperLogLog(){
+
+}
+
+HyperLogLog::~HyperLogLog(){;}
---
+++ HyperLogLog.h
@@ -0,0 +1,10 @@
+#ifndef HYPERLOGLOG_H
+#define HYPERLOGLOG_H
+
+class HyperLogLog{
+    HyperLogLog();
+    ~HyperLogLog();
+    
+};
+
+#endif
