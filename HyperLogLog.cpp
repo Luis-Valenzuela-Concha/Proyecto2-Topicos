a8e56959822f8a256584bbd77b62ed9c73e8b9d1
Bruno Arce R <brunoarce0@gmail.com>
2023-11-01 17:35:08 -0300

    Agregadas correcciones estimación
    Puse los 3 casos de if

--- HyperLogLog.cpp
+++ HyperLogLog.cpp
@@ -1,20 +1,23 @@
+#include "HyperLogLog.h"
+
 #include <bits/stdc++.h>
+
 #include <iostream>
 #include <unordered_map>
-#include "HyperLogLog.h"
+
 #include "MurmurHash3.h"
 
 int seed = 1;
 
 HyperLogLog::HyperLogLog(int p) {
     this->p = p;
-    m = pow(2, p); //Tamaño de vector M
+    m = pow(2, p);  // Tamaño de vector M
     M = vector<unsigned int>(m, 0);
 }
 
-HyperLogLog::~HyperLogLog() {;}
+HyperLogLog::~HyperLogLog() { ; }
 
-//Trasforma elemento en j y w 
+// Trasforma elemento en j y w
 pair<unsigned int, unsigned int> HyperLogLog::values(string element) {
     uint32_t x;
     MurmurHash3_x86_32(element.c_str(), element.size(), seed, &x);
@@ -28,45 +31,69 @@ pair<unsigned int, unsigned int> HyperLogLog::values(string element) {
         w++;
     }
     pair<unsigned int, unsigned int> values;
-    values.first = j;  //posicion
-    values.second = w; //valor
+    values.first = j;   // posicion
+    values.second = w;  // valor
 
     return values;
 }
 
 void HyperLogLog::insert(string element) {
     pair<unsigned int, unsigned int> valores = values(element);
-    unsigned int j = valores.first;  //posicion
-    unsigned int w = valores.second; //valor
-    M[j]=max(M[j],w);
+    unsigned int j = valores.first;   // posicion
+    unsigned int w = valores.second;  // valor
+    M[j] = max(M[j], w);
 }
 
 long double HyperLogLog::estimarCard() {
     long double E;
 
-    long double a_m = 0.7213 / (1 + 1.079/m);
-    long double num = a_m * m*m;
-    long double den = 0;    
-    
+    long double a_m = 0.7213 / (1 + 1.079 / m);
+    long double num = a_m * m * m;
+    long double den = 0;
+
     for (int i = 0; i < m; i++) {
         den += 1 / (pow(2, M[0]));
     }
     E = num / den;
-    //Agregar correcciones
-    return E;
+    // Agregar correcciones
+    long double E_asterisco;
+
+    if (E <= 5 / 2 * m) {
+        int V = 0;
+        for (int i = 0; i < m; i++) {
+            if (M[i] == 0) {
+                V++;
+            }
+        }
+        if (V != 0) {
+            E_asterisco = m * log(m / V);
+        } else {
+            E_asterisco = E;
+        }
+    }
+
+    if (E <= 1 / 30 * pow(2, 32)) {
+        E_asterisco = E;
+    }
+
+    if (E > 1 / 30 * pow(2, 32)) {
+        E_asterisco = -1 * pow(2, 32) * log(1 - E / pow(2, 32));
+    }
+    return E_asterisco;
 }
 
-void HyperLogLog::Union(HyperLogLog h) { //Se une con otro sketch
-    if(this->m != h.m){
-        cout << "Deben ser de mismo tamaño << endl"; return;
+void HyperLogLog::Union(HyperLogLog h) {  // Se une con otro sketch
+    if (this->m != h.m) {
+        cout << "Deben ser de mismo tamaño << endl";
+        return;
     }
-    for (int i=0; i<this->m; i++) {
+    for (int i = 0; i < this->m; i++) {
         this->M[i] = max(this->M[i], h.M[i]);
     }
 }
 
-void HyperLogLog::print(){
-    for(int i=0;i<m;i++){
-        cout << M[i] <<" ";
+void HyperLogLog::print() {
+    for (int i = 0; i < m; i++) {
+        cout << M[i] << " ";
     }
 }
