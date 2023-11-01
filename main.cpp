bd03ef8b101a891a066b910c78f8072f1d55a2b7
Luis-Valenzuela-Concha <lvalenzuel2020@udec.cl>
2023-11-01 15:21:12 -0300

    terminado

--- main.cpp
+++ main.cpp
@@ -6,9 +6,8 @@
 
 using namespace std;
 
-int main(){
-    // Copia los valores en un arreglo
-    ifstream archivo("GCF_001522075.1_ASM152207v1_genomic.fna");
+vector<string> copiarArchivo(string nombreArchivo){
+    ifstream archivo(nombreArchivo);
     vector<string> vec;
     string linea;
     while (getline(archivo, linea)) {
@@ -16,24 +15,34 @@ int main(){
         vec.push_back(linea);
     }
     archivo.close();
-    //Fin copia
+    return vec;
+}
 
+void insertarKMER(HyperLogLog *h, vector<string> v,int k){
+    for(int i = 0; i < v.size(); i++){
+        if(v[i].size() < k) continue; //Si el k es mas grande que el string continua
+        for(int j = 0; j < v[i].size()-k+1; j++){
+            string kmer = v[i].substr(j,k);
+            h->insert(kmer);
+        }
+    }
+}
+
+int main(){
+    vector<string> genoma1 = copiarArchivo("GCF_001522075.1_ASM152207v1_genomic.fna");
+    vector<string> genoma2 = copiarArchivo("GCF_000717965.1_ASM71796v1_genomic.fna");
+    
     //Inicializacion
     HyperLogLog HLL(14);
     HyperLogLog HLL2(14);
 
     //Inserta kmer
-    int k = 10;
-    for(int i = 0; i < vec.size(); i++){
-        if(vec[i].size() < k) continue; //Si el kmer es mas grande que el string continua
-        for(int j = 0; j < vec[i].size()-k+1; j++){
-            string kmer = vec[i].substr(j,k);
-            HLL.insert(kmer);
-            HLL2.insert(kmer);
-        }
-    }
+    insertarKMER(&HLL,genoma1,10);
+    insertarKMER(&HLL2,genoma2,11);
+
     printf("Cardinalidad HLL : %.2Lf\n",HLL.estimarCard());
     printf("Cardinalidad HLL2: %.2Lf\n",HLL2.estimarCard());
+
     HLL.Union(HLL2);
     printf("Cardinalidad HLL : %.2Lf\n",HLL.estimarCard());
     return 0;
